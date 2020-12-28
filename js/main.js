92b13fda44184f7c9a2175cb46034fd4393bc248
Tiago C. Brandão <tiago_curvelo@hotmail.com>
2020-12-28 01:52:43 -0300

    Primiero commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+/*
+var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count ++;
+};
+*/
+/*
+var age = prompt("Qual sua idade?");
+if (age >= 18) {
+    alert("Maior de idade")
+} else {
+    alert("Menor de idade")
+};
+*/
+
+
+/*
+var fruits = [{name:"apple", color:"red"}, {name:"grape", color:"purple"}];
+console.log(fruits);
+alert(fruits[1].name);
+*/
+/*
+var fruit = {name:"apple", color:"red"};
+console.log(fruit.name);
+alert(fruit.color);
+*/
+
+
+//var list = ["apple", "pear", "orange"];
+//list.push("grape");
+//list.pop();
+
+//console.log(list);
+//console.log(list.toString());
+//console.log(list.join(" - "));
+
+//var name = "Tiago Curvelo";
+//var n1 = 5;
+//var n2 = 10;
+//var phrase = "Japan is the best team in the world";
+//alert(name + " tem " + age + " anos");
+//alert(age + age2);
+//console.log(name);
+//console.log(n1 + n2);
+//console.log(phrase.toLowerCase());
